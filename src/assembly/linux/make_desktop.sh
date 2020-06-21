996d467e42bd177da922047bc75d0164a5a59cf3
Igor Maznitsa <rrg4400@gmail.com>
2020-06-21 15:17:00 +0300

    fixed logo and desktop

--- src/assembly/linux/make_desktop.sh
+++ src/assembly/linux/make_desktop.sh
@@ -13,12 +13,26 @@ echo GenericName=Ravikoodi >> $TARGET
 echo Exec=$RAVIKOODI_HOME/run.sh >> $TARGET
 echo Terminal=false >> $TARGET
 echo Type=Application >> $TARGET
-echo Icon=$RAVIKOODI_HOME/icon.png >> $TARGET
-echo Categories=Application; >> $TARGET
+echo Icon=$RAVIKOODI_HOME/icon.svg >> $TARGET
+echo "Categories=Application;" >> $TARGET
+echo "Keywords=kodi;ravi;koodi;tv;stream;" >> $TARGET
 echo StartupWMClass=RavikoodiServer >> $TARGET
 echo StartupNotify=true >> $TARGET
 
-
-chmod +x $TARGET
-
 echo Desktop script has been generated: $TARGET
+
+if [ -d ~/.gnome/apps ]; then
+    echo copy to ~/.gnome/apps
+    cp -f $TARGET ~/.gnome/apps
+fi
+
+if [ -d ~/.local/share/applications ]; then
+    echo copy to ~/.local/share/applications
+    cp -f $TARGET ~/.local/share/applications
+fi
+
+if [ -d ~/Desktop ]; then
+    echo copy to ~/Desktop
+    cp -f $TARGET ~/Desktop
+fi
+
