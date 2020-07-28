ebc213caaa3a81406778b741be6245c667d6b4ef
Igor Maznitsa <rrg4400@gmail.com>
2020-07-28 21:59:26 +0300

    fixed compatibility with KODI 18.xx API

--- src/main/java/com/igormaznitsa/ravikoodi/kodijsonapi/ActivePlayerInfo.java
+++ src/main/java/com/igormaznitsa/ravikoodi/kodijsonapi/ActivePlayerInfo.java
@@ -1,10 +1,12 @@
 package com.igormaznitsa.ravikoodi.kodijsonapi;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor(force = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ActivePlayerInfo {
   private final long playerid;
   private final String type;
